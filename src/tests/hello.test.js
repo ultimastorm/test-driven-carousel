94168a18f0cb377b6428dc20a8173fab935e27a8
Stefan Orban <stefan.orban@asperitas-solutions.com>
2020-04-04 17:55:49 +0300

    :wrench: Initial Wallaby setup

--- src/tests/hello.test.js
+++ src/tests/hello.test.js
@@ -4,6 +4,8 @@ describe('JSX', () => {
   it('calls React.createElement', () => {
     const createElementSpy = jest.spyOn(React, 'createElement');
     <h1>Hello, JSX!</h1>;
-    expect(createElementSpy).toHaveBeenCalledWith('h1', null, 'Hello, JSX!');
+
+    expect(createElementSpy)
+      .toHaveBeenCalledWith('h1', null, 'Hello, JSX!');
   });
 });
---
+++ wallaby.js
@@ -0,0 +1,13 @@
+module.exports = function () {
+  return {
+    testFramework: 'jest',
+    env: {
+      type: 'node',
+    },
+    tests: ['src/tests/**/*.test.js'],
+    files: ['src/**/*.js', '!**/*.test.js', '!**/.*'],
+    compilers: {
+      '**/*.js': wallaby.compilers.babel(),
+    },
+  };
+};
