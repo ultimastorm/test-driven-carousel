4ec83941c26046840bfa6de5cf015250f26e0f91
Stefan Orban <stefan.orban@asperitas-solutions.com>
2020-04-04 19:57:26 +0300

    :sparkles: Initial implementation of a CarouselSlide

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: ['react'],
   extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module',
