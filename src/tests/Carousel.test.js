480d4aefd54ec7512123e15e9476d708e8ada84c
Stefan Orban <stefan.orban@asperitas-solutions.com>
2020-04-05 17:03:03 +0300

    :sparkles: Add prop for extending the <img> component

--- src/tests/Carousel.test.js
+++ src/tests/Carousel.test.js
@@ -92,5 +92,21 @@ describe('Carousel', () => {
       expect(wrapper.state('slideIndex')).toBe(0);
     });
   });
+
+  it('passes defaultImg and defultImgHeight to the CarouselSlide', () => {
+    const defaultImg = () => 'test';
+    const defaultImgHeight = 1234;
+    wrapper.setProps({ defaultImg, defaultImgHeight });
+    expect(wrapper.find(CarouselSlide).prop('Img')).toBe(defaultImg);
+    expect(wrapper.find(CarouselSlide).prop('imgHeight')).toBe(defaultImgHeight);
+  });
+
+  it('allows individual slides to override Img and imgHeight', () => {
+    const Img = () => 'test';
+    const imgHeight = 1234;
+    wrapper.setProps({ slides: [{...slides[0], Img, imgHeight}] });
+    expect(wrapper.find(CarouselSlide).prop('Img')).toBe(Img);
+    expect(wrapper.find(CarouselSlide).prop('imgHeight')).toBe(imgHeight);
+  });
 });
 
